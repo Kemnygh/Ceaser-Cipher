027cb26b7347e3078306f6334fdadb3ae3c53f7f
Kemuel Nalifuma <kemnygh@gmail.com>
2022-05-07 09:25:02 +0300

    Add encoding/decoding parameter and the subsequent test

--- src/main/java/models/CeaserCipher.java
+++ src/main/java/models/CeaserCipher.java
@@ -4,15 +4,19 @@ public class CeaserCipher {
 
     private final String userInput;
     private final int shiftNumber;
+    private final String cipherConverter;
 
-    public CeaserCipher(String userInput, int shiftNumber) {
+    public CeaserCipher(String userInput, int shiftNumber, String cipherConverter) {
         this.userInput = userInput;
         this.shiftNumber = shiftNumber;
+        this.cipherConverter = cipherConverter;
     }
 
     public String getUserInput() { return this.userInput;}
 
     public int getShiftNumber() { return this.shiftNumber;}
+
+    public String getCipherConverter() {return this.cipherConverter;}
 
     public String isString() { return userInput.getClass().getSimpleName();}
 
@@ -20,5 +24,7 @@ public class CeaserCipher {
         Integer newShiftNumber = shiftNumber;
         return newShiftNumber.getClass().getSimpleName();
     }
+
+
 
 }
--- src/test/java/models/CeaserCipherTest.java
+++ src/test/java/models/CeaserCipherTest.java
@@ -8,22 +8,27 @@ class CeaserCipherTest {
 
     @Test
     void newCeaserCipher_instantiates_correctly() {
-        CeaserCipher newCipher = new CeaserCipher("User input", 1);
+        CeaserCipher newCipher = new CeaserCipher("User input", 1, "encode");
         assertTrue(newCipher instanceof CeaserCipher);
     }
 
     @Test
     void newCeaserCipher_getsUserInput() {
-        CeaserCipher testInputType = new CeaserCipher("User input", 1);
+        CeaserCipher testInputType = new CeaserCipher("User input", 1, "decode");
         assertEquals(testInputType.isString(), testInputType.getUserInput().getClass().getSimpleName());
     }
 
     @Test
     void newCeaserCipher_getsShiftNumber() {
-        CeaserCipher testShiftNumber = new CeaserCipher("User input", 1);
+        CeaserCipher testShiftNumber = new CeaserCipher("User input", 1, "encode");
         Integer wrappedShiftNumber = testShiftNumber.getShiftNumber();
         assertEquals(testShiftNumber.isInteger(), wrappedShiftNumber.getClass().getSimpleName());
     }
 
+    @Test
+    void newCeaserCipher_getsCipherConverter() {
+        CeaserCipher testConverterType = new CeaserCipher("User input", 1, "decode");
+        assertEquals(testConverterType.isString(), testConverterType.getCipherConverter().getClass().getSimpleName());
+    }
 
 }
