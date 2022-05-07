2e2a527e3eb8825e0669618e66cc01512297c8a0
Kemuel Nalifuma <kemnygh@gmail.com>
2022-05-07 09:14:43 +0300

    Add cipher model and tests

--- src/main/java/models/CeaserCipher.java
+++ src/main/java/models/CeaserCipher.java
@@ -1,4 +1,24 @@
 package models;
 
 public class CeaserCipher {
+
+    private final String userInput;
+    private final int shiftNumber;
+
+    public CeaserCipher(String userInput, int shiftNumber) {
+        this.userInput = userInput;
+        this.shiftNumber = shiftNumber;
+    }
+
+    public String getUserInput() { return this.userInput;}
+
+    public int getShiftNumber() { return this.shiftNumber;}
+
+    public String isString() { return userInput.getClass().getSimpleName();}
+
+    public String isInteger() {
+        Integer newShiftNumber = shiftNumber;
+        return newShiftNumber.getClass().getSimpleName();
+    }
+
 }
--- src/test/java/models/CeaserCipherTest.java
+++ src/test/java/models/CeaserCipherTest.java
@@ -1,7 +1,29 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CeaserCipherTest {
+
+    @Test
+    void newCeaserCipher_instantiates_correctly() {
+        CeaserCipher newCipher = new CeaserCipher("User input", 1);
+        assertTrue(newCipher instanceof CeaserCipher);
+    }
+
+    @Test
+    void newCeaserCipher_getsUserInput() {
+        CeaserCipher testInputType = new CeaserCipher("User input", 1);
+        assertEquals(testInputType.isString(), testInputType.getUserInput().getClass().getSimpleName());
+    }
+
+    @Test
+    void newCeaserCipher_getsShiftNumber() {
+        CeaserCipher testShiftNumber = new CeaserCipher("User input", 1);
+        Integer wrappedShiftNumber = testShiftNumber.getShiftNumber();
+        assertEquals(testShiftNumber.isInteger(), wrappedShiftNumber.getClass().getSimpleName());
+    }
+
 
 }
